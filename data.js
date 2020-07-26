a09f0597dbb8e74f87848a84901f38fd49953d8c
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-07-26 21:40:46 +0800

    Update data.js

--- data.js
+++ data.js
@@ -6,9 +6,7 @@ if(guv("filename")=="jsq2.25.zip"&guv("d")=="file"&guv("type")=="download"){
 var href="/cdn/file/download/jsq2.25.zip";open(href);
 document.documentElement.innerHTML="Downloading..."
 setTimeout(function(){
-sessionStorage.downloadhref=href;
-sessionStorage.downloadname="jsq.zip"
-location.href="file/download/success";
+location.href="file/download/success?dname=jsq.zip&durl="+encodeURIComponent(href);
 },1)
 return "return";
 }});
