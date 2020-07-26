0b878a3433ecd77d3ea7dbf574afd73f70d25f3a
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-07-26 16:08:42 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,37 +1,9 @@
 (function(){
 window.datas=[];
-/****
-====Operation instruction====
-#Start note#====Usage method====
-Ordinary method:
-datas.push({To be perfected})
-Advanced method:
-datas.push({function:function(){}})
-====Description====
-Method for passing in data(@data "The data you want to enter"):
-[1]https://shc7432.github.io/cdn/get?
---------
-Description of ordinary method:
-@name "Url Data" 
-@url "Redirect to Url" 
-Description of advanced method:
-[1]The function can customize the judgment according to the data in the url.
-[2]#End note#Return "break" can stop judging.
-====Simple explanation====
-datas.push(function(){
-
-})
-====The end====
-****/
-//Start the ordinary method
+//Start push
 datas.push(function(){
 if(guv("filename")=="jsq.zip"&guv("d")=="file"&guv("type")=="download"){
 location.href="file/download/jsq.zip";
-//documentdocumentElementinnerHTML
-}
 })
-
-//Stop the ordinary method and start the advanced method
-
-//End
+//END
 })()
