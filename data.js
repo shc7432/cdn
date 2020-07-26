ea806bb4e64f4951616aafccf389575d18104767
shc0743 <60457042+shc0743@users.noreply.github.com>
2020-07-26 16:10:38 +0800

    Update data.js

--- data.js
+++ data.js
@@ -2,8 +2,8 @@
 window.datas=[];
 //Start push
 datas.push(function(){
-if(guv("filename")=="jsq.zip"&guv("d")=="file"&guv("type")=="download"){
-location.href="file/download/jsq.zip";
+if(guv("filename")=="jsq2.25.zip"&guv("d")=="file"&guv("type")=="download"){
+location.href="file/download/jsq2.25.zip";
 })
 //END
 })()
