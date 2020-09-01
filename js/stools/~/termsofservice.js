b501be0f25ab2c0c61c5288a43db37bb81dcda5e
shc0743 <shc0743@outlook.com>
2020-09-01 12:17:12 +0800

    Update termsofservice.js

--- js/stools/~/termsofservice.js
+++ js/stools/~/termsofservice.js
@@ -2,7 +2,7 @@
 var a=document.createElement("div");
 a.title="服务条款";
 a.innerHTML=`请仔细阅读服务条款,然后选择是否同意条款。
-<div style="border:1px solid #ccc;">
+<div style="border:1px solid #ccc;overflow:scroll;">
 
 </div>`
 })()
