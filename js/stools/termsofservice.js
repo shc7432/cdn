9adef85560d9110f81efed8f1358c02637876fc7
shc0743 <shc0743@outlook.com>
2020-09-06 16:05:57 +0800

    Update termsofservice.js

--- js/stools/termsofservice.js
+++ js/stools/termsofservice.js
@@ -5,7 +5,7 @@ if(location.href.indexOf("https://shc7432.github.io/")==0){let a=document.create
 var a=document.createElement("div");
 a.title="服务条款";
 a.innerHTML=`请仔细阅读服务条款,然后选择是否同意条款。
-<pre style="border:1px solid #ccc;overflow:auto;width:95%;height:65%;"><center><b>服务条款</b></center>
+<pre style="border:1px solid #ccc;overflow:auto;width:95%;height:65%;position:relative;left:2.5%;"><center><b>服务条款</b></center>
 <b>第一条</b> 为保证您(以下简称“用户”)和"https://shc7432.github.io"网站(以下简称“本网站”)(包括其服务)的合法权益,防止不合法的情况发生,特制定本服务条款。
 <b>第二条</b> 您可以选择拒绝本《服务条款》(以下简称“本条款”),但本网站(或其服务)可能无法使用。
 <b>第三条</b> 您有权随时取消您对本条款的授权,但请注意,根据上一条,我们有权不向您提供服务。
