112b6c6ba78e4434784dd060a88a037fe1a28a12
shc0743 <shc0743@outlook.com>
2020-09-06 12:49:36 +0800

    Update termsofservice.js

--- js/stools/~/termsofservice.js
+++ js/stools/~/termsofservice.js
@@ -1,12 +1,53 @@
 (function(){
 if(localStorage.agreeToTermsOfService||!new RegExp('zh-CN','i').test(navigator.language)) return false;
+if(localStorage.agreeToTermsOfService===false){delete window.sTools;delete window.gadgetsInDomainShc7432;
+if(location.href.indexOf("https://shc7432.github.io/")==0){let a=document.createElement("div");a.setAttribute('style','width:100%;height:100%;');document.body.append(a)};return false;};
 var a=document.createElement("div");
 a.title="服务条款";
 a.innerHTML=`请仔细阅读服务条款,然后选择是否同意条款。
-<pre style="border:1px solid #ccc;overflow:auto;"><center><b>服务条款</b></center>
+<pre style="border:1px solid #ccc;overflow:auto;width:95%;height:65%;"><center><b>服务条款</b></center>
 <b>第一条</b> 为保证您(以下简称“用户”)和"https://shc7432.github.io"网站(以下简称“本网站”)(包括其服务)的合法权益,防止不合法的情况发生,特制定本服务条款。
 <b>第二条</b> 您可以选择拒绝本《服务条款》(以下简称“本条款”),但本网站(或其服务)可能无法使用。
-<b>第三条</b> 您可以在<a href="https://shc7432.github.io/cdn/js/stools/~/termsofservice.html">https://shc7432.github.io/cdn/js/stools/~/termsofservice.html</a>随时查看本条款。
-</pre><br><button onclick="">同意</button> &nbsp; <button onclick="">不同意</button>`
-
+<b>第三条</b> 您有权随时取消您对本条款的授权,但请注意,根据上一条,我们有权不向您提供服务。
+        提示:在<a href="https://shc7432.github.io/cdn/js/stools/~/termsofservice.html#cancel">这里</a>取消授权(单击右键可以复制链接)
+<b>第四条</b> 您可以在<a href="https://shc7432.github.io/cdn/js/stools/~/termsofservice.html">https://shc7432.github.io/cdn/js/stools/~/termsofservice.html</a>随时查看本条款。
+<b>第五条</b> 本条款与法律有冲突的,以法律为准。
+<b>第六条</b> 我们有权在法律范围内使用您的这些信息,您可以立即授权,也可以待需要使用时授权:
+        1) 位置信息;
+        2) cookie与本地存储;
+        3) 法律许可的其他信息.
+<b>第七条</b> 使用规则<pre style="height:800px;overflow:auto;"><b>1. 用户在使用本网站的服务时，必须遵守《网络安全法》等中华人民共和国相关法律法规的规定，用户应同意将不会利用本服务进行任何违法或不正当的活动，包括但不限于下列行为:
+（1）上载、展示、张贴、传播或以其它方式传送含有下列内容之一的信息：
+　　1）反对宪法所确定的基本原则的；
+　　2）危害国家安全，泄露国家秘密，颠覆国家政权，破坏国家统一的；
+　　3）损害国家荣誉和利益的；
+　　4）煽动民族仇恨、民族歧视、破坏民族团结的；
+　　5）破坏国家宗教政策，宣扬邪教和封建迷信的；
+　　6）散布谣言，扰乱社会秩序，破坏社会稳定的；
+　　7）散布淫秽、色情、赌博、暴力、凶杀、恐怖或者教唆犯罪的；
+　　8）侮辱或者诽谤他人，侵害他人合法权利的；
+　　9）含有虚假、有害、胁迫、侵害他人隐私、骚扰、侵害、中伤、粗俗、猥亵、或其它道德上令人反感的内容；
+　　10）含有中国法律、法规、规章、条例以及任何具有法律效力之规范所限制或禁止的其它内容的；
+（2）不得为任何非法目的而使用网络服务系统；
+（3）不利用本网站的服务从事以下活动：
+　　1) 未经允许，进入计算机信息网络或者使用计算机信息网络资源的；
+　　2) 未经允许，对计算机信息网络功能进行删除、修改或者增加的；
+　　3) 未经允许，对进入计算机信息网络中存储、处理或者传输的数据和应用程序进行删除、修改或者增加的；
+　　4) 故意制作、传播计算机病毒等破坏性程序的；
+　　5) 其他危害计算机信息网络安全的行为。
+2. 用户违反本协议或相关的服务条款的规定，导致或产生的任何第三方主张的任何索赔、要求或损失，包括合理的律师费，您同意赔偿本网站与合作公司、关联公司，并使之免受损害。对此，本网站有权视用户的行为性质，采取包括但不限于删除用户发布信息内容、暂停使用许可、终止服务、限制使用、回收帐号、追究法律责任等措施。
+</b>3. 用户不得对本服务任何部分或本服务之使用或获得，进行复制、拷贝、出售、转售或用于任何其它商业目的。
+4. 用户须对自己在使用本服务过程中的行为承担法律责任。用户承担法律责任的形式包括但不限于：对受到侵害者进行赔偿，以及在本网站首先承担了因用户行为导致的行政处罚或侵权损害赔偿责任后，用户应给予本网站等额的赔偿。
+5. 用户在使用本网站服务时遵守以下互联网底线：
+　　1）法律法规底线
+　　2）社会主义制度底线
+　　3）国家利益底线
+　　4）公民合法权益底线
+　　5）社会公共秩序底线
+　　6）道德风尚底线
+　　7）信息真实性底线</pre>
+<b>第八条</b> <b>未经允许,您不得对本网站及其服务进行反编制、反汇编等行为。</b>
+</pre><br><button onclick="localStorage.agreeToTermsOfService=true;location.reload(!!1)">同意</button> &nbsp; <button onclick="localStorage.agreeToTermsOfService=false;location.reload(!!1);">不同意</button>`
+document.body.appendChild(a);
+a.dialog({modal:1;autoOpen:1;hidex:1;width:75%;});
 })()
