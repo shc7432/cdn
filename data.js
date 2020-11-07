e3f8d95aa26edb46dd3b0aa86a9e26a7e002638f
shc0743 <shc0743@outlook.com>
2020-11-07 13:08:10 +0800

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@
 window.datas=[];
 //Start push
 datas.push(function(){
-if(guv("filename")=="jsq2.24.zip"&guv("d")=="file"&guv("type")=="download"){ 
+if(guv("filename")=="jsq2.25.zip"&guv("d")=="file"&guv("type")=="download"){ 
 var href="/cdn/file/download/jsq2.25.zip";var atag=document.createElement("a");
 atag.href=href;atag.target="_blank";atag.hidden=1;atag.download="jsq.zip";
 document.documentElement.appendChild(atag);atag.click();
