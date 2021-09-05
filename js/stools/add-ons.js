10df61474d9f9e7d127009fa18a2c1e829eca4dc
shc0743 <60457042+shc0743@users.noreply.github.com>
2021-09-05 17:47:32 +0800

    Update add-ons.js

--- js/stools/add-ons.js
+++ js/stools/add-ons.js
@@ -3,7 +3,8 @@ gadgetsInDomainShc7432.addItem('open_url_in_blank',function(url, download = fals
     a.href = url;
     a.target = '_blank';
     a.download = (download ? download : undefined);
-    a.style.display = 'none !important';
+    a.style.position = 'fixed !important';
+    a.style.top = '-114514% !important';
     document.body.appendChild(a);
     a.click();
     a.remove();
