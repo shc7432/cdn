90a52cfe03cf490e416b15e0a385109b591278bf
shc0743 <60457042+shc0743@users.noreply.github.com>
2021-09-12 21:31:10 +0800

    Update add-ons.js

--- js/stools/add-ons.js
+++ js/stools/add-ons.js
@@ -1,4 +1,4 @@
-gadgetsInDomainShc7432.addItem('open_url_in_blank',function(url, download = false){
+gadgetsInDomainShc7432.addItem('open_url_in_blank', function (url, download = false) {
     var a = document.createElement('a');
     a.href = url;
     a.target = '_blank';
@@ -8,4 +8,25 @@ gadgetsInDomainShc7432.addItem('open_url_in_blank',function(url, download = fals
     document.body.appendChild(a);
     a.click();
     a.remove();
+});
+
+gadgetsInDomainShc7432.addItem('toFullScreen', function (elem) {
+    if (elem.requestFullscreen) {
+        return elem.requestFullscreen();
+    } else if (elem.webkitRequestFullScreen) {
+        return elem.webkitRequestFullScreen();
+    } else if (elem.mozRequestFullScreen) {
+        return elem.mozRequestFullScreen();
+    } else {
+        return elem.msRequestFullscreen();
+    }
+});
+gadgetsInDomainShc7432.addItem('unFullScreen', function () {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen();
+    }
 });
